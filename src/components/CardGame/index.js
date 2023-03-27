8570e9a9774c6bb4e10e6bb4ca6fa4666ca3d3cc
ClecioSousa00 <cleciosouza015@gmail.com>
2023-03-27 12:44:44 -0300

    instalado o vitejs

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame (){
+    return `
+        <article class="card-game">
+            <img src="src/components/images/alura-pixel 1.svg" alt="Logo Alura">
+        </article>
+    `
+}
+
+export default CardGame
