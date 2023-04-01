c0d54844795b97846bfe17b87d0b719526b86111
ClecioSousa00 <cleciosouza015@gmail.com>
2023-04-01 19:27:47 -0300

    change player

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,14 +1,17 @@
 import CardGame from "../CardGame"
 import "./style.css"
 
- window.handleClick = event => {
-    const cardSelect = event.target.closest('.card-front-back')
-    cardSelect.classList.toggle('active')
-}
+ 
 function CardFrontBack(icon, alt){
    
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = event => {
+        const cardSelect = event.target.closest('.card-front-back')
+        cardSelect.classList.toggle('active')
+    }
+
     return /*html*/`
-        <article class="card-front-back" onclick="handleClick(event)">
+        <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
             <div class="card-front">
                 ${CardGame()}
             </div>
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,6 +3,21 @@ import CardGame from "../../components/CardGame";
 import "./style.css"
 
 function BoardGame(amountCards){
+    window.boardGame = {}
+    window.boardGame.handleClick = ()=>{
+        const boardGame = document.querySelector('.board-game')
+        const cardsActive = boardGame.querySelectorAll('.card-front-back.active')
+        const arrowDown = document.querySelector('.arrow-down')
+        if(cardsActive.length === 2){
+            setTimeout(() => {
+                cardsActive.forEach(card => card.classList.remove('active'))
+                const currentPlayer = arrowDown.getAttribute('data-current-player')
+                arrowDown.setAttribute('data-current-player',currentPlayer == 1 ? 2 : 1)
+
+            }, 1000);
+        }
+    }
+
     const cards =[
         {
             icon: "Javascript",
@@ -21,7 +36,7 @@ function BoardGame(amountCards){
     const htmlCardsList = cards.map(card => CardFrontBack(card.icon, card.alt))
     const htmlCards = htmlCardsList.join('')
     return /*html*/`
-        <section class="board-game">
+        <section class="board-game" onclick="boardGame.handleClick()">
             ${htmlCards}
         </section>
     `
